6c16234690b2b563a2e1deadabf614e0e55b81e2
Sachin Goyal <sachin@fanduel.com>
2019-12-16 22:44:53 +0000

    added navigation

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import AppNavigator from './navigation/AppNavigator';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+      <AppNavigator />
     </View>
   );
 }
