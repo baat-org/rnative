8d93f555ae362eca554ae76c2ad3edf738382b7f
Sachin Goyal <sachin@fanduel.com>
2019-12-23 23:58:27 +0000

    added validate token logic

--- screens/login/CheckAuthScreen.js
+++ screens/login/CheckAuthScreen.js
@@ -5,6 +5,7 @@ import {
     StatusBar,
     View,
 } from 'react-native';
+import { GQL_API_URI } from 'react-native-dotenv';
 
 class CheckAuthScreen extends React.Component {
     componentDidMount() {
@@ -14,7 +15,32 @@ class CheckAuthScreen extends React.Component {
     _bootstrapAsync = async () => {
         const userToken = await AsyncStorage.getItem('userToken');
 
-        this.props.navigation.navigate(userToken ? 'Home' : 'Auth');
+        if (!userToken) {
+            this.props.navigation.navigate('Auth');
+        } else {
+            fetch(GQL_API_URI,
+                {
+                    method: 'POST',
+                    headers: {
+                        Accept: 'application/json',
+                        'Content-Type': 'application/json',
+                    },
+                    body: '{"query": "mutation { validateUserToken ( userToken: \\\"' + userToken + '\\\")}", "variables": null}',
+                })
+                .then((response) => response.json())
+                .then((responseJson) => {
+                    if (responseJson && responseJson.data && responseJson.data.validateUserToken) {
+                        AsyncStorage.setItem('userToken', responseJson.data.validateUserToken);
+                        this.props.navigation.navigate('Home');
+                    } else {
+                        this.props.navigation.navigate('Auth');
+                    }
+                })
+                .catch((error) => {
+                    console.error(error);
+                    this.props.navigation.navigate('Auth');
+                });
+        }
     };
 
     // Render any loading content that you like here
