bde2854ef463ca06a05a2039f9c9d6e6a1a36e6e
Sachin Goyal <sachin@fanduel.com>
2019-12-28 23:28:04 +0000

    good header

--- screens/home/ChatScreen.js
+++ screens/home/ChatScreen.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import { Text, Button, View } from 'react-native';
-import { SafeAreaView } from 'react-navigation';
-import GlobalStyles from '../../GlobalStyles'
+import { Header } from 'react-native-elements';
 
 class ChatScreen extends React.Component {
   render() {
     this.props.navigation.closeDrawer();
-    
+
     return (
-      <SafeAreaView style={GlobalStyles.safeArea}>
-        <Button
-          onPress={() => this.props.navigation.toggleDrawer()}
-          title="Show Users"
-        />
-        <View>
-          <Text>
-            {this.props.navigation.getParam('user')}
-          </Text>
-        </View>
-      </SafeAreaView>
+      <Header
+        leftComponent={{ icon: 'menu', color: '#fff', onPress: this.props.navigation.toggleDrawer }}
+        centerComponent={{ text: this.props.navigation.getParam('user'), style: { color: '#fff' } }}
+      />
     );
   }
 }
