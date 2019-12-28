b368fb8f042ed8d5fe356c6f0f1eb4123f937987
Sachin Goyal <sachin@fanduel.com>
2019-12-28 23:37:21 +0000

    signout

--- screens/home/ChatScreen.js
+++ screens/home/ChatScreen.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import { Header } from 'react-native-elements';
+import { AsyncStorage } from 'react-native';
 
 class ChatScreen extends React.Component {
+  signOut() {
+    AsyncStorage.clear();
+    this.props.navigation.navigate('Auth');
+  }
+
   render() {
     this.props.navigation.closeDrawer();
 
@@ -9,6 +15,7 @@ class ChatScreen extends React.Component {
       <Header
         leftComponent={{ icon: 'menu', color: '#fff', onPress: this.props.navigation.toggleDrawer }}
         centerComponent={{ text: this.props.navigation.getParam('user'), style: { color: '#fff' } }}
+        rightComponent={{ text: 'Sign out', style: { color: '#fff' }, onPress: () => this.signOut() }}
       />
     );
   }
