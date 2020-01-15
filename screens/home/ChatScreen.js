b084882753ddbfdcdd99c29aec7a9d3a3aba87bf
Sachin Goyal <sachin@fanduel.com>
2020-01-15 23:33:01 +0000

    nailed fetching

--- screens/home/ChatScreen.js
+++ screens/home/ChatScreen.js
@@ -2,24 +2,75 @@ import React from 'react';
 import { Header } from 'react-native-elements';
 import { AsyncStorage, View, Text, TextInput } from 'react-native';
 import { ScrollView } from 'react-native';
+import { GQL_API_URI } from 'react-native-dotenv';
+import GlobalStyles from '../../GlobalStyles';
 
 class ChatScreen extends React.Component {
-  signOut() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      errorMessage: '',
+    }
+  };
+
+  componentDidMount() {
+    this._loadChatPanels().then(chatPanels => {
+      this.chatPanels = chatPanels;
+    });
+  }
+
+
+  _loadChatPanels = async () => {
+    const userToken = await AsyncStorage.getItem('userToken');
+    const chatPanels = {};
+
+    await fetch(GQL_API_URI,
+      {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: '{"query": "{ users ( userToken: \\\"' + userToken + '\\\") { id } }", "variables": null, "operationName":null}',
+      })
+      .then((response) => response.json())
+      .then((responseJson) => {
+        if (responseJson && responseJson.data && responseJson.data.users) {
+          const users = responseJson.data.users;
+          for (let i = 0; i < users.length; i++) {
+            chatPanels[users[i].id] = <Text>{users[i].id}</Text>
+          }
+        }
+      })
+      .catch((error) => {
+        this.setState({ errorMessage: error });
+      });
+
+      return chatPanels;
+  };
+
+  _signOut() {
     AsyncStorage.clear();
     this.props.navigation.navigate('Auth');
   }
 
   render() {
     this.props.navigation.closeDrawer();
+    const recipientUserId = this.props.navigation.getParam('recipientUserId');
+    const reciepientFullName = this.props.navigation.getParam('reciepientFullName');
+    const chatPanel = recipientUserId && this.chatPanels && this.chatPanels[recipientUserId] ? this.chatPanels[recipientUserId] : <Text>Please select user from side menu.</Text>;
 
     return (
       <View style={{ flex: 1 }}>
         <Header
           leftComponent={{ icon: 'menu', color: '#fff', onPress: this.props.navigation.toggleDrawer }}
-          centerComponent={{ text: this.props.navigation.getParam('user'), style: { color: '#fff' } }}
-          rightComponent={{ text: 'Sign out', style: { color: '#fff' }, onPress: () => this.signOut() }}
+          centerComponent={{ text: reciepientFullName, style: { color: '#fff' } }}
+          rightComponent={{ text: 'Sign out', style: { color: '#fff' }, onPress: () => this._signOut() }}
         />
-        <ScrollView><Text>Chat Content goes here</Text></ScrollView>
+        <Text style={GlobalStyles.error}>{this.state.errorMessage}</Text>
+        <ScrollView>
+          {chatPanel}
+        </ScrollView>
         <View>
           <TextInput multiline={true} numberOfLines={4} placeholder="Message" />
           <Text>Bottom</Text>
--- screens/home/SideMenuScreen.js
+++ screens/home/SideMenuScreen.js
@@ -2,29 +2,62 @@ import React from 'react';
 import { SafeAreaView } from 'react-navigation';
 import GlobalStyles from '../../GlobalStyles'
 import { ListItem } from 'react-native-elements';
+import { AsyncStorage } from 'react-native';
+import { GQL_API_URI } from 'react-native-dotenv';
 
 class SideMenuScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      recipientUsers: [],
+    }
+  };
+
+  componentDidMount() {
+    this._loadRecipientUsers().then(recipientUsers => {
+      this.setState({ recipientUsers: recipientUsers });
+    });
+  }
+
+
+  _loadRecipientUsers = async () => {
+    const userToken = await AsyncStorage.getItem('userToken');
+    let recipientUsers = [];
+
+    await fetch(GQL_API_URI,
+      {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: '{"query": "{ users ( userToken: \\\"' + userToken + '\\\") { id, email, fullName, avatarUrl } }", "variables": null, "operationName":null}',
+      })
+      .then((response) => response.json())
+      .then((responseJson) => {
+        if (responseJson && responseJson.data && responseJson.data.users) {
+          recipientUsers = responseJson.data.users;
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    return recipientUsers;
+  };
+
   render() {
+    const recpientPanels = this.state.recipientUsers.map((recipientUser, key) =>
+      <ListItem
+        key={key}
+        onPress={() => this.props.navigation.navigate('Chat', { recipientUserId: recipientUser.id, reciepientFullName: recipientUser.fullName })}
+        title={recipientUser.fullName}
+        bottomDivider
+      />
+    );
     return (
       <SafeAreaView style={GlobalStyles.safeArea}>
-        <ListItem
-          key={1}
-          onPress={() => this.props.navigation.navigate('Chat', { user: 'User 1' })}
-          title="User 1"
-          bottomDivider
-        />
-        <ListItem
-          key={2}
-          onPress={() => this.props.navigation.navigate('Chat', { user: 'User 2' })}
-          title="User 2"
-          bottomDivider
-        />
-        <ListItem
-          key={3}
-          onPress={() => this.props.navigation.navigate('Chat', { user: 'User 3' })}
-          title="User 3"
-          bottomDivider
-        />
+        {recpientPanels}
       </SafeAreaView>
     );
   }
